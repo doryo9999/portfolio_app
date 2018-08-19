08bb1838faba52088854d9a61c7f4b03fff129b3
Doryo9999 <ditrymo6745@gmail.com>
2018-08-19 02:45:29 +0000

    13.4.1 uploader Picture

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,13 +16,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
-    assert_select 'div.alert'
-    assert_select 'form[action="/signup"]'
+    assert_select 'div.field_with_errors'
+    # assert_select 'div.alert' 
+    # assert_select 'form[action="/signup"]'
   end
   
   test "valid signup information with account activation" do
     get signup_path
-    assert_difference "User.count", 1 do
+    assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User",
                                  email: "user@example.com",
                                  password:              "password",
