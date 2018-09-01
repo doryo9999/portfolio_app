5e817a0773bf5312152a9aff2ca5559cfa9add2b
Doryo9999 <ditrymo6745@gmail.com>
2018-09-01 07:56:18 +0000

    Add title search

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 [*Ruby on Rails チュートリアル*](https://railstutorial.jp/)
 [Michael Hartl](http://www.michaelhartl.com/) 著
 
+## 拡張  
+### 自己紹介文の追加  
+自己紹介文の表示と編集機能追加→[Rails Tutorial に自己紹介文の機能拡張をしてみた \- Qiita](https://qiita.com/doryo9999/items/29ae123b38d49a985c9d)  
+### Micropostにタイトル追加。  
+
 ## ライセンス
 
 [Ruby on Rails チュートリアル](https://railstutorial.jp/)内にある
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,6 +6,15 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
   validates :title,   presence: true, length: { maximum: 100 }
+  default_scope -> { order(created_at: :desc) }
+
+  def self.search(search) #ここでのself.はMicropost.を意味する
+    if search
+      Micropost.where(['title LIKE ?', "%#{sanitize_sql_like(search)}%"]) #検索とcontentの部分一致を表示。
+    else
+      Micropost.all 
+    end
+  end
   
   private
   
